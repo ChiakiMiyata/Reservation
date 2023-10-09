752b2d517c03fb0d343b3109eeee5e51671c256a
ChiakiMiyata <mycaymkz15@gmail.com>
2023-10-10 00:53:31 +0900

    [add]top トップ画面を追加、ログアウト機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_09_25_161850) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
