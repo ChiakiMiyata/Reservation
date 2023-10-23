003bbdea201764b128154f62b2e52a5ef05c5012
ChiakiMiyata <mycaymkz15@gmail.com>
2023-10-24 01:17:02 +0900

    [add]施設登録時のバリデーションの追加、show #35 戻るボタンの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
   has_one_attached :room_image
+  validates :room_name, :introduction, :address, presence: true
+  validates :fee, numericality: true, presence: true
 end
