50517005d609a2bf5271cb0af56e2ea2ac34eaf0
ChiakiMiyata <mycaymkz15@gmail.com>
2023-10-24 13:26:36 +0900

    [new] Reservationモデルとコントローラを作成

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable, and :omniauthable
-  has_one_attached :avatar
-  devise :database_authenticatable, :registerable, :rememberable, :validatable
-
-  has_many :romms, dependent: :destroy
-
-end
