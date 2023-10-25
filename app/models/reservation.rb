9add9833aa267bd18aaa40f9ffd9acde3e81ecb6
ChiakiMiyata <mycaymkz15@gmail.com>
2023-10-25 12:21:25 +0900

    [add] 予約時のバリデーションを追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,16 @@
 class Reservation < ApplicationRecord
-  belongs_to :user
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates :people, presence: true,numericality: true
+  validate :start_end_check
+
   belongs_to :room
+  belongs_to :user
+
+  def start_end_check
+    if (self.check_in != nil && self.check_out != nil)
+      errors.add(:check_out, "はチェックインより後の日付を選択してください") unless
+      self.check_in < self.check_out
+    end
+  end
 end
